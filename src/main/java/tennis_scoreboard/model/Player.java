2b8e55f441466b4ff93dd2ceea8e7202da394ac0
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-21 21:47:54 +0500

    Created Player.java class

---
+++ src/main/java/tennis_scoreboard/model/Player.java
@@ -0,0 +1,39 @@
+package tennis_scoreboard.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "players")
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "name")
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
