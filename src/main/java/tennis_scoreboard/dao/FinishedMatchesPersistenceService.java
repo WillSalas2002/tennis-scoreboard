9bac3c640c30f2152013e721578ca152705f916d
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-25 22:54:44 +0500

    Update: added some methods for retrieving data from db

--- src/main/java/tennis_scoreboard/dao/FinishedMatchesPersistenceService.java
+++ src/main/java/tennis_scoreboard/dao/FinishedMatchesPersistenceService.java
@@ -3,10 +3,18 @@ package tennis_scoreboard.dao;
 import tennis_scoreboard.model.Match;
 import tennis_scoreboard.model.Player;
 
+import java.util.List;
+
 public interface FinishedMatchesPersistenceService {
     void saveMatch(Match match);
 
     void savePlayers(Player player);
 
     Player getPlayerByName(String name);
+
+    long getCountInMatchTable();
+
+    List<Match> getTotalMatches(int offset, int limit);
+
+    List<Match> getMatchesByPlayerName(String nameToLookFor, int offset, int limit);
 }
