d3767fc07a5575dc892409552a2e4d646af3665d
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-25 22:57:57 +0500

    Created MatchesSerlvet.java class for with logic to display finished matches

---
+++ src/main/java/tennis_scoreboard/controller/MatchesServlet.java
@@ -0,0 +1,47 @@
+package tennis_scoreboard.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import tennis_scoreboard.dao.FinishedMatchesPersistenceService;
+import tennis_scoreboard.dao.FinishedMatchesPersistenceServiceImpl;
+import tennis_scoreboard.model.Match;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/matches/*")
+public class MatchesServlet extends HttpServlet {
+    private final FinishedMatchesPersistenceService service = new FinishedMatchesPersistenceServiceImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String pageStr = req.getParameter("page");
+        String nameToLookFor = req.getParameter("filter_by_player_name");
+
+        int currentPage = 1;
+
+        if (pageStr != null) {
+            currentPage = Integer.parseInt(pageStr);
+        }
+
+        long totalMatchesCount = service.getCountInMatchTable();
+        int matchesPerPage = 4;
+        double pages = Math.ceil(((double) totalMatchesCount) / matchesPerPage);
+
+        List<Match> matchList;
+
+        if (nameToLookFor != null && !nameToLookFor.isBlank()) {
+            matchList = service.getMatchesByPlayerName(nameToLookFor.trim(), (currentPage - 1) * matchesPerPage, matchesPerPage);
+        } else {
+            matchList = service.getTotalMatches((currentPage - 1) * matchesPerPage, matchesPerPage);
+        }
+
+        req.setAttribute("matchList", matchList);
+
+        req.getRequestDispatcher("/view/matches-list.jsp").forward(req, resp);
+    }
+}
