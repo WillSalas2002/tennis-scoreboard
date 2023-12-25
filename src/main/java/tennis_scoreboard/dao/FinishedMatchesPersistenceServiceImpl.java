73105cd684d66246318a10f557419321ab04a1ec
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-25 22:56:02 +0500

    Feat: Implemented methods from defined in service

--- src/main/java/tennis_scoreboard/dao/FinishedMatchesPersistenceServiceImpl.java
+++ src/main/java/tennis_scoreboard/dao/FinishedMatchesPersistenceServiceImpl.java
@@ -3,9 +3,10 @@ package tennis_scoreboard.dao;
 import jakarta.persistence.NoResultException;
 import org.hibernate.HibernateError;
 import org.hibernate.Session;
-import org.hibernate.query.Query;
 import tennis_scoreboard.model.Match;
 import tennis_scoreboard.model.Player;
+
+import java.util.List;
 
 
 public class FinishedMatchesPersistenceServiceImpl implements FinishedMatchesPersistenceService {
@@ -58,5 +59,51 @@ public class FinishedMatchesPersistenceServiceImpl implements FinishedMatchesPer
         }
 
         return player;
+    }
+
+    @Override
+    public long getCountInMatchTable() {
+
+        long count = 0;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            count = session.createQuery("SELECT COUNT(*) FROM Match", Long.class).getSingleResult();
+        }
+
+        return count;
+    }
+
+    @Override
+    public List<Match> getTotalMatches(int offset, int limit) {
+
+        List<Match> matchList;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            matchList = session.createQuery("FROM Match", Match.class).setFirstResult(offset).setMaxResults(limit).getResultList();
+            session.getTransaction().commit();
+        }
+
+        return matchList;
+    }
+
+    @Override
+    public List<Match> getMatchesByPlayerName(String nameToLookFor, int offset, int limit) {
+
+        List<Match> matchList;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            matchList = session.createQuery("FROM Match m WHERE m.player1.name = :nameToLookFor OR m.player2.name = :nameToLookFor", Match.class)
+                    .setParameter("nameToLookFor", nameToLookFor)
+                    .setFirstResult(offset)
+                    .setMaxResults(limit)
+                    .getResultList();
+            session.getTransaction().commit();
+        }
+
+        return matchList;
+
     }
 }
